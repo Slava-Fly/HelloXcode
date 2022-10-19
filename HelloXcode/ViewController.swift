c8032c4184dba57982c9680d07186b5348f0a7f4
@Slava-Fly <school.94@bk.ru>
2022-10-19 15:46:41 +0800

    Initial Commit

---
+++ HelloXcode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloXcode
+//
+//  Created by User on 19.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
